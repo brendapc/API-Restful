fae612f8333559e70c93098bb4e3b4ad1d2fb4c5
Brenda Pereira <brendacamara@outlook.com>
2020-06-02 19:12:57 -0300

    done

--- server.js
+++ server.js
@@ -17,21 +17,42 @@ o resource aqui é o client
 use todos os resources num padrão:  tudo singular ou plural
 */
 app.get('/clients', function(req,res){
-    res.json(data)
+    res.json(data);
 })
 
 
 app.get('/clients/id:', function(req,res){
-    
+    const { id } = req.params;
+    const client = data.find(cli=> cli.id == id);
+
+    if(!client) return res.status(204).json();
+
+    res.json(client)
 })
 app.post('/clients',function(req,res){
+    const {name , email} = req.body;
 
+    //salvar
+    res.json({name, email});
 } )
 app.put('/clients/:id', function(req,res){
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+
+    if(!client) return res.status(204).json();
+
+    const { name } = req.body;
+
+    client.name = name;
+
+    res.json(client);
 
 })
 app.delete('/clients/:id', function(req,res){
+    const { id } = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
 
+    res.json(clientsFiltered);
 })
 
 app.listen(3000, function(){
